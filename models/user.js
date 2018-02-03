e2eb55a4abd6a478f4e64fd425b8509cada0c0b2
Paige Lo <sk8asd123@gmail.com>
2018-02-03 06:26:22 -0600

    removing Schema from user.js

--- models/user.js
+++ models/user.js
@@ -3,18 +3,18 @@
 "use strict";
 
 
-const mongoose = require("mongoose");
-
-let userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-});
-
-module.exports = mongoose.model("User", userSchema);
+//const mongoose = require("mongoose");
+//
+//let userSchema = new Schema({
+//    username: {
+//        type: String,
+//        required: true
+//    },
+//    password: {
+//        type: String,
+//        required: true
+//    }
+//});
+//
+//module.exports = mongoose.model("User", userSchema);
 
